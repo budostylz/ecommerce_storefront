a1087460335d7619e289cd36c90af7fcde8a0f33
Budo <budo@MacBookAir.attlocal.net>
2025-08-14 20:46:08 -0400

    commit

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -3,8 +3,61 @@ import React from "react";
 const Categories = () => {
   return (
     <section className="categories">
+      {/* Design Tokens */}
+      <style>{`
+        :root {
+          /* Large Category - Title */
+          --categories-large-title-font-family-home-1: Montserrat, sans-serif;
+          --categories-large-title-font-size-home-1: 48px;
+          --categories-large-title-font-weight-home-1: 700;
+          --categories-large-title-color-home-1: #111111;
+          --categories-large-title-text-align-home-1: left;
+
+          /* Large Category - Paragraph */
+          --categories-large-text-font-family-home-1: "Roboto", sans-serif;
+          --categories-large-text-font-size-home-1: 16px;
+          --categories-large-text-font-weight-home-1: 400;
+          --categories-large-text-color-home-1: #444444;
+          --categories-large-text-line-height-home-1: 1.5;
+          --categories-large-text-text-align-home-1: left;
+
+          /* Large Category - Link */
+          --categories-large-link-font-family-home-1: Montserrat, sans-serif;
+          --categories-large-link-font-size-home-1: 14px;
+          --categories-large-link-font-weight-home-1: 600;
+          --categories-large-link-color-home-1: #111111;
+          --categories-large-link-text-decoration-home-1: none;
+
+          /* Large Category - Padding */
+          --categories-large-padding-left-home-1: 60px;
+          --categories-large-padding-top-home-1: 40px;
+
+          /* Small Category - Title */
+          --categories-small-title-font-family-home-1: Montserrat, sans-serif;
+          --categories-small-title-font-size-home-1: 20px;
+          --categories-small-title-font-weight-home-1: 600;
+          --categories-small-title-color-home-1: #111111;
+          --categories-small-title-text-align-home-1: left;
+
+          /* Small Category - Paragraph */
+          --categories-small-text-font-family-home-1: "Roboto", sans-serif;
+          --categories-small-text-font-size-home-1: 14px;
+          --categories-small-text-font-weight-home-1: 400;
+          --categories-small-text-color-home-1: #444444;
+          --categories-small-text-text-align-home-1: left;
+
+          /* Small Category - Link */
+          --categories-small-link-font-family-home-1: Montserrat, sans-serif;
+          --categories-small-link-font-size-home-1: 12px;
+          --categories-small-link-font-weight-home-1: 600;
+          --categories-small-link-color-home-1: #111111;
+          --categories-small-link-text-decoration-home-1: none;
+        }
+      `}</style>
+
       <div className="container-fluid">
         <div className="row">
+          {/* Large Category */}
           <div className="col-lg-6 p-0">
             <div
               className="categories__item categories__large__item set-bg"
@@ -16,82 +69,111 @@ const Categories = () => {
               <div
                 className="categories__text"
                 style={{
-                  paddingLeft: "60px", // shift horizontally
-                  paddingTop: "40px", // optional vertical adjustment
+                  paddingLeft: "var(--categories-large-padding-left-home-1)",
+                  paddingTop: "var(--categories-large-padding-top-home-1)",
                 }}
               >
-                <h1>Women’s fashion</h1>
-                <p>
+                <h1
+                  style={{
+                    fontFamily: "var(--categories-large-title-font-family-home-1)",
+                    fontSize: "var(--categories-large-title-font-size-home-1)",
+                    fontWeight: "var(--categories-large-title-font-weight-home-1)",
+                    color: "var(--categories-large-title-color-home-1)",
+                    ["textAlign" as any]: "var(--categories-large-title-text-align-home-1)",
+                  }}
+                >
+                  Women’s fashion
+                </h1>
+                <p
+                  style={{
+                    fontFamily: "var(--categories-large-text-font-family-home-1)",
+                    fontSize: "var(--categories-large-text-font-size-home-1)",
+                    fontWeight: "var(--categories-large-text-font-weight-home-1)",
+                    color: "var(--categories-large-text-color-home-1)",
+                    lineHeight: "var(--categories-large-text-line-height-home-1)",
+                    ["textAlign" as any]: "var(--categories-large-text-text-align-home-1)",
+                  }}
+                >
                   Sitamet, consectetur adipiscing elit, sed do eiusmod tempor
                   incidid-unt labore edolore magna aliquapendisse ultrices
                   gravida.
                 </p>
-                <a href="#">Shop now</a>
+                <a
+                  href="#"
+                  style={{
+                    fontFamily: "var(--categories-large-link-font-family-home-1)",
+                    fontSize: "var(--categories-large-link-font-size-home-1)",
+                    fontWeight: "var(--categories-large-link-font-weight-home-1)",
+                    color: "var(--categories-large-link-color-home-1)",
+                    textDecoration:
+                      "var(--categories-large-link-text-decoration-home-1)",
+                  }}
+                >
+                  Shop now
+                </a>
               </div>
             </div>
           </div>
+
+          {/* Small Categories */}
           <div className="col-lg-6">
             <div className="row">
-              <div className="col-lg-6 col-md-6 col-sm-6 p-0">
+              {[
+                { title: "Men’s fashion", items: "358 items", img: "category-2.jpg" },
+                { title: "Kid’s fashion", items: "273 items", img: "category-3.jpg" },
+                { title: "Cosmetics", items: "159 items", img: "category-4.jpg" },
+                { title: "Accessories", items: "792 items", img: "category-5.jpg" },
+              ].map((cat, idx) => (
                 <div
-                  className="categories__item set-bg"
-                  style={{
-                    backgroundImage:
-                      "url('src/origin/base/web/img/categories/category-2.jpg')",
-                  }}
+                  key={idx}
+                  className="col-lg-6 col-md-6 col-sm-6 p-0"
                 >
-                  <div className="categories__text">
-                    <h4>Men’s fashion</h4>
-                    <p>358 items</p>
-                    <a href="#">Shop now</a>
+                  <div
+                    className="categories__item set-bg"
+                    style={{
+                      backgroundImage: `url('src/origin/base/web/img/categories/${cat.img}')`,
+                    }}
+                  >
+                    <div className="categories__text">
+                      <h4
+                        style={{
+                          fontFamily: "var(--categories-small-title-font-family-home-1)",
+                          fontSize: "var(--categories-small-title-font-size-home-1)",
+                          fontWeight: "var(--categories-small-title-font-weight-home-1)",
+                          color: "var(--categories-small-title-color-home-1)",
+                          ["textAlign" as any]: "var(--categories-small-title-text-align-home-1)",
+                        }}
+                      >
+                        {cat.title}
+                      </h4>
+                      <p
+                        style={{
+                          fontFamily: "var(--categories-small-text-font-family-home-1)",
+                          fontSize: "var(--categories-small-text-font-size-home-1)",
+                          fontWeight: "var(--categories-small-text-font-weight-home-1)",
+                          color: "var(--categories-small-text-color-home-1)",
+                          ["textAlign" as any]: "var(--categories-small-text-text-align-home-1)",
+                        }}
+                      >
+                        {cat.items}
+                      </p>
+                      <a
+                        href="#"
+                        style={{
+                          fontFamily: "var(--categories-small-link-font-family-home-1)",
+                          fontSize: "var(--categories-small-link-font-size-home-1)",
+                          fontWeight: "var(--categories-small-link-font-weight-home-1)",
+                          color: "var(--categories-small-link-color-home-1)",
+                          textDecoration:
+                            "var(--categories-small-link-text-decoration-home-1)",
+                        }}
+                      >
+                        Shop now
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-lg-6 col-md-6 col-sm-6 p-0">
-                <div
-                  className="categories__item set-bg"
-                  style={{
-                    backgroundImage:
-                      "url('src/origin/base/web/img/categories/category-3.jpg')",
-                  }}
-                >
-                  <div className="categories__text">
-                    <h4>Kid’s fashion</h4>
-                    <p>273 items</p>
-                    <a href="#">Shop now</a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-6 col-md-6 col-sm-6 p-0">
-                <div
-                  className="categories__item set-bg"
-                  style={{
-                    backgroundImage:
-                      "url('src/origin/base/web/img/categories/category-4.jpg')",
-                  }}
-                >
-                  <div className="categories__text">
-                    <h4>Cosmetics</h4>
-                    <p>159 items</p>
-                    <a href="#">Shop now</a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-6 col-md-6 col-sm-6 p-0">
-                <div
-                  className="categories__item set-bg"
-                  style={{
-                    backgroundImage:
-                      "url('src/origin/base/web/img/categories/category-5.jpg')",
-                  }}
-                >
-                  <div className="categories__text">
-                    <h4>Accessories</h4>
-                    <p>792 items</p>
-                    <a href="#">Shop now</a>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
--- src/utils/overlayMap.ts
+++ src/utils/overlayMap.ts
@@ -97,7 +97,7 @@ export const overlayMap = {
         "--footer-copyright-heart-color-global-0"
       ]
     },
-      header: {
+    header: {
       component: "Header",
       props: {
         logo: "https://storage.googleapis.com/budoapps-5aacf.firebasestorage.app/templates/ecommerce/fashio/logo.png",
@@ -161,7 +161,325 @@ export const overlayMap = {
       ]
     },
   },
-'/contact': {
+  "/": {
+    banner: {
+      id: "component-banner",
+      component: "Banner",
+      position: {
+        x: 0,
+        y: "auto",
+        width: "100%",
+        zIndex: 1
+      },
+      props: {
+        slides: [
+          { title: "The Project Jacket", subtitle: "The Chloe Collection", linkText: "Shop Now" },
+          { title: "The Project Jacket", subtitle: "The Chloe Collection", linkText: "Shop Now" },
+          { title: "The Project Jacket", subtitle: "The Chloe Collection", linkText: "Shop Now" }
+        ],
+        backgroundImage: "src/origin/base/web/img/banner/banner-1.jpg",
+        loop: true,
+        align: "center",
+        autoplayMs: 3000
+      },
+      tokens: [
+        /* Slider alignment */
+        "--banner-slider-text-align-home-1",
+
+        /* Span (subtitle) */
+        "--banner-text-span-font-family-home-1",
+        "--banner-text-span-font-size-home-1",
+        "--banner-text-span-color-home-1",
+        "--banner-text-span-font-weight-home-1",
+
+        /* H1 (title) */
+        "--banner-text-h1-font-family-home-1",
+        "--banner-text-h1-font-size-home-1",
+        "--banner-text-h1-color-home-1",
+        "--banner-text-h1-font-weight-home-1",
+
+        /* Link */
+        "--banner-text-link-color-home-1",
+        "--banner-text-link-font-weight-home-1",
+
+        /* Dots */
+        "--banner-dots-dot-background-color-home-1",
+        "--banner-dots-dot-background-color-home-2"
+      ],
+      editable: true
+    },
+    categories: {
+      id: "component-categories",
+      component: "Categories",
+      position: {
+        x: 0,
+        y: "auto",
+        width: "100%",
+        zIndex: 1
+      },
+      props: {
+        largeCategory: {
+          title: "Women’s fashion",
+          description:
+            "Sitamet, consectetur adipiscing elit, sed do eiusmod tempor incidid-unt labore edolore magna aliquapendisse ultrices gravida.",
+          linkLabel: "Shop now",
+          backgroundImage: "src/origin/base/web/img/categories/category-1.jpg"
+        },
+        smallCategories: [
+          { title: "Men’s fashion", items: "358 items", img: "src/origin/base/web/img/categories/category-2.jpg" },
+          { title: "Kid’s fashion", items: "273 items", img: "src/origin/base/web/img/categories/category-3.jpg" },
+          { title: "Cosmetics", items: "159 items", img: "src/origin/base/web/img/categories/category-4.jpg" },
+          { title: "Accessories", items: "792 items", img: "src/origin/base/web/img/categories/category-5.jpg" }
+        ]
+      },
+      tokens: [
+        /* Large Category - Title */
+        "--categories-large-title-font-family-home-1",
+        "--categories-large-title-font-size-home-1",
+        "--categories-large-title-font-weight-home-1",
+        "--categories-large-title-color-home-1",
+        "--categories-large-title-text-align-home-1",
+
+        /* Large Category - Paragraph */
+        "--categories-large-text-font-family-home-1",
+        "--categories-large-text-font-size-home-1",
+        "--categories-large-text-font-weight-home-1",
+        "--categories-large-text-color-home-1",
+        "--categories-large-text-line-height-home-1",
+        "--categories-large-text-text-align-home-1",
+
+        /* Large Category - Link */
+        "--categories-large-link-font-family-home-1",
+        "--categories-large-link-font-size-home-1",
+        "--categories-large-link-font-weight-home-1",
+        "--categories-large-link-color-home-1",
+        "--categories-large-link-text-decoration-home-1",
+
+        /* Large Category - Padding */
+        "--categories-large-padding-left-home-1",
+        "--categories-large-padding-top-home-1",
+
+        /* Small Category - Title */
+        "--categories-small-title-font-family-home-1",
+        "--categories-small-title-font-size-home-1",
+        "--categories-small-title-font-weight-home-1",
+        "--categories-small-title-color-home-1",
+        "--categories-small-title-text-align-home-1",
+
+        /* Small Category - Paragraph */
+        "--categories-small-text-font-family-home-1",
+        "--categories-small-text-font-size-home-1",
+        "--categories-small-text-font-weight-home-1",
+        "--categories-small-text-color-home-1",
+        "--categories-small-text-text-align-home-1",
+
+        /* Small Category - Link */
+        "--categories-small-link-font-family-home-1",
+        "--categories-small-link-font-size-home-1",
+        "--categories-small-link-font-weight-home-1",
+        "--categories-small-link-color-home-1",
+        "--categories-small-link-text-decoration-home-1"
+      ],
+      editable: true
+    },
+    discount: {
+      id: "component-discount",
+      component: "Discount",
+      position: { x: 0, y: "auto", width: "100%", zIndex: 1 },
+      props: {
+        title: {
+          span: "Discount",
+          h2: "Summer 2019",
+          h5: { prefix: "Sale", suffix: "50%" }
+        },
+        countdown: [
+          { num: "22", label: "Days" },
+          { num: "18", label: "Hour" },
+          { num: "46", label: "Min" },
+          { num: "05", label: "Sec" }
+        ],
+        image: "src/origin/base/web/img/discount.jpg",
+        linkLabel: "Shop now"
+      },
+      tokens: [
+        "--discount-title-span-font-family-home-1",
+        "--discount-title-span-font-size-home-1",
+        "--discount-title-span-color-home-1",
+        "--discount-title-span-font-weight-home-1",
+        "--discount-title-span-text-align-home-1",
+        "--discount-title-h2-font-family-home-1",
+        "--discount-title-h2-font-size-home-1",
+        "--discount-title-h2-color-home-1",
+        "--discount-title-h2-font-weight-home-1",
+        "--discount-title-h2-text-align-home-1",
+        "--discount-title-h5-font-family-home-1",
+        "--discount-title-h5-font-size-home-1",
+        "--discount-title-h5-color-home-1",
+        "--discount-title-h5-font-weight-home-1",
+        "--discount-title-h5-text-align-home-1",
+        "--discount-title-h5-span-color-home-1",
+        "--discount-title-h5-span-font-weight-home-1",
+        "--discount-countdown-span-font-family-home-1",
+        "--discount-countdown-span-font-size-home-1",
+        "--discount-countdown-span-color-home-1",
+        "--discount-countdown-span-font-weight-home-1",
+        "--discount-countdown-span-text-align-home-1",
+        "--discount-countdown-p-font-family-home-1",
+        "--discount-countdown-p-font-size-home-1",
+        "--discount-countdown-p-color-home-1",
+        "--discount-countdown-p-font-weight-home-1",
+        "--discount-countdown-p-text-align-home-1",
+        "--discount-link-font-family-home-1",
+        "--discount-link-font-size-home-1",
+        "--discount-link-color-home-1",
+        "--discount-link-font-weight-home-1",
+        "--discount-link-background-color-home-1",
+        "--discount-link-text-align-home-1"
+      ],
+      editable: true
+    },
+    product: {
+      id: "component-product",
+      component: "Product",
+      position: { x: 0, y: "auto", width: "100%", zIndex: 1 },
+      props: {
+        sectionTitle: "New product",
+        filters: [
+          { label: "All", filter: "*", active: true },
+          { label: "Women’s", filter: ".women" },
+          { label: "Men’s", filter: ".men" },
+          { label: "Kid’s", filter: ".kid" },
+          { label: "Accessories", filter: ".accessories" },
+          { label: "Cosmetics", filter: ".cosmetic" }
+        ],
+        items: [
+          { img: "src/origin/base/web/img/product/product-1.jpg", label: "New", title: "Buttons tweed blazer", price: "$ 59.0", oldPrice: null, category: "women", stars: 5 },
+          { img: "src/origin/base/web/img/product/product-2.jpg", label: null, title: "Flowy striped skirt", price: "$ 49.0", oldPrice: null, category: "men", stars: 5 },
+          { img: "src/origin/base/web/img/product/product-3.jpg", label: "out of stock", title: "Cotton T-Shirt", price: "$ 59.0", oldPrice: null, category: "accessories", stars: 5 },
+          { img: "src/origin/base/web/img/product/product-4.jpg", label: null, title: "Slim striped pocket shirt", price: "$ 59.0", oldPrice: null, category: "cosmetic", stars: 5 },
+          { img: "src/origin/base/web/img/product/product-5.jpg", label: null, title: "Fit micro corduroy shirt", price: "$ 59.0", oldPrice: null, category: "kid", stars: 5 },
+          { img: "src/origin/base/web/img/product/product-6.jpg", label: "Sale", title: "Tropical Kimono", price: "$ 49.0", oldPrice: "$ 59.0", category: "women men kid accessories cosmetic", stars: 5 },
+          { img: "src/origin/base/web/img/product/product-7.jpg", label: null, title: "Contrasting sunglasses", price: "$ 59.0", oldPrice: null, category: "women men kid accessories cosmetic", stars: 5 },
+          { img: "src/origin/base/web/img/product/product-8.jpg", label: "Sale", title: "Water resistant backpack", price: "$ 49.0", oldPrice: "$ 59.0", category: "women men kid accessories cosmetic", stars: 5 }
+        ]
+      },
+      tokens: [
+        "--product-section-title-h4-font-family-home-1",
+        "--product-section-title-h4-font-size-home-1",
+        "--product-section-title-h4-color-home-1",
+        "--product-section-title-h4-font-weight-home-1",
+        "--product-section-title-h4-text-align-home-1",
+        "--product-filter-li-font-family-home-1",
+        "--product-filter-li-font-size-home-1",
+        "--product-filter-li-color-home-1",
+        "--product-filter-li-font-weight-home-1",
+        "--product-item-text-text-align-home-1",
+        "--product-item-title-font-family-home-1",
+        "--product-item-title-font-size-home-1",
+        "--product-item-title-color-home-1",
+        "--product-item-title-font-weight-home-1",
+        "--product-item-rating-color-home-1",
+        "--product-item-price-color-home-1",
+        "--product-item-price-font-weight-home-1",
+        "--product-item-oldprice-color-home-1",
+        "--product-label-new-background-color-home-1",
+        "--product-label-sale-background-color-home-1",
+        "--product-label-stockout-background-color-home-1",
+        "--product-label-text-color-home-1",
+        "--product-label-font-weight-home-1"
+      ],
+      editable: true
+    },
+    services: {
+      id: "component-services",
+      component: "Services",
+      position: { x: 0, y: "auto", width: "100%", zIndex: 1 },
+      props: {
+        items: [
+          { icon: "fa-car", title: "Free Shipping", text: "For all oder over $99" },
+          { icon: "fa-money", title: "Money Back Guarantee", text: "If good have Problems" },
+          { icon: "fa-support", title: "Online Support 24/7", text: "Dedicated support" },
+          { icon: "fa-headphones", title: "Payment Secure", text: "100% secure payment" },
+        ]
+      },
+      tokens: [
+        "--services-item-icon-color-home-1",
+        "--services-item-icon-text-align-home-1",
+        "--services-item-h6-font-family-home-1",
+        "--services-item-h6-font-size-home-1",
+        "--services-item-h6-color-home-1",
+        "--services-item-h6-font-weight-home-1",
+        "--services-item-h6-text-align-home-1",
+        "--services-item-p-font-family-home-1",
+        "--services-item-p-font-size-home-1",
+        "--services-item-p-color-home-1",
+        "--services-item-p-font-weight-home-1",
+        "--services-item-p-text-align-home-1",
+      ],
+      editable: true
+    },
+    trend: {
+      id: "component-trend",
+      component: "Trend",
+      position: { x: 0, y: "auto", width: "100%", zIndex: 1 },
+      props: {
+        columns: [
+          {
+            heading: "Hot Trend",
+            items: [
+              { img: "src/origin/base/web/img/trend/ht-1.jpg", title: "Chain bucket bag", price: "$ 59.0", stars: 5 },
+              { img: "src/origin/base/web/img/trend/ht-2.jpg", title: "Pendant earrings", price: "$ 59.0", stars: 5 },
+              { img: "src/origin/base/web/img/trend/ht-3.jpg", title: "Cotton T-Shirt", price: "$ 59.0", stars: 5 }
+            ]
+          },
+          {
+            heading: "Best seller",
+            items: [
+              { img: "src/origin/base/web/img/trend/bs-1.jpg", title: "Cotton T-Shirt", price: "$ 59.0", stars: 5 },
+              { img: "src/origin/base/web/img/trend/bs-2.jpg", title: "Zip-pockets pebbled tote briefcase", price: "$ 59.0", stars: 5 },
+              { img: "src/origin/base/web/img/trend/bs-3.jpg", title: "Round leather bag", price: "$ 59.0", stars: 5 }
+            ]
+          },
+          {
+            heading: "Feature",
+            items: [
+              { img: "src/origin/base/web/img/trend/f-1.jpg", title: "Bow wrap skirt", price: "$ 59.0", stars: 5 },
+              { img: "src/origin/base/web/img/trend/f-2.jpg", title: "Metallic earrings", price: "$ 59.0", stars: 5 },
+              { img: "src/origin/base/web/img/trend/f-3.jpg", title: "Flap cross-body bag", price: "$ 59.0", stars: 5 }
+            ]
+          }
+        ]
+      },
+      tokens: [
+        "--trend-section-title-h4-font-family-home-1",
+        "--trend-section-title-h4-font-size-home-1",
+        "--trend-section-title-h4-color-home-1",
+        "--trend-section-title-h4-font-weight-home-1",
+        "--trend-section-title-h4-text-align-home-1",
+        "--trend-item-h6-font-family-home-1",
+        "--trend-item-h6-font-size-home-1",
+        "--trend-item-h6-color-home-1",
+        "--trend-item-h6-font-weight-home-1",
+        "--trend-item-h6-text-align-home-1",
+        "--trend-rating-star-color-home-1",
+        "--trend-rating-text-align-home-1",
+        "--trend-price-font-family-home-1",
+        "--trend-price-font-size-home-1",
+        "--trend-price-color-home-1",
+        "--trend-price-font-weight-home-1",
+        "--trend-price-text-align-home-1",
+        "--trend-column-heading-font-family-home-1",
+        "--trend-column-heading-font-size-home-1",
+        "--trend-column-heading-color-home-1",
+        "--trend-column-heading-font-weight-home-1",
+        "--trend-column-heading-text-align-home-1",
+        "--trend-item-background-color-home-1"
+      ],
+      editable: true
+    }
+
+  },
+  '/contact': {
     contact: {
       id: 'component-contact',
       component: 'Contact',
@@ -172,14 +490,14 @@ export const overlayMap = {
         zIndex: 1
       },
       props: {
-        contactInfo:'Contact info',
-        sendMessageLabel:'SEND MESSAGE',
-        sendMessageButton:'Send Message Button',
+        contactInfo: 'Contact info',
+        sendMessageLabel: 'SEND MESSAGE',
+        sendMessageButton: 'Send Message Button',
         addressLabel: 'Address',
         address: '66 West Flagler St, Miami, Florida, 33130',
-        phoneLabel:'Phone',
+        phoneLabel: 'Phone',
         phones: ['125-711-811', '125-668-886'],
-        supportLabel:'Support',
+        supportLabel: 'Support',
         supportEmail: 'Support.photography@gmail.com',
         mapSrc:
           'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3586.547649012345!2d-80.1950148236409!3d25.774265911945164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9b69f4d0d69cf%3A0x28a55d3ad739888!2s66%20W%20Flagler%20St%2C%20Miami%2C%20FL%2033130!5e0!3m2!1sen!2sus!4v1691777777777!5m2!1sen!2sus'
